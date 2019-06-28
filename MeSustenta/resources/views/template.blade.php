c603ba2579d7a4a3a28a40f7bbf5b0e8ac4d3544
CPBeisiegel <camila.beisiegel@gmail.com>
2019-06-28 16:31:37 -0300

    organização das blades e criação do auth

--- MeSustenta/resources/views/template.blade.php
+++ MeSustenta/resources/views/template.blade.php
@@ -9,6 +9,11 @@
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/style.css')}}" >
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/contato.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/cadastro.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/sobre.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/produto.css')}}">
+    <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/pageProdutos.css')}}">
     <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/app.css')}}" >
     <title>MeSustenta</title>
 </head>
@@ -210,7 +215,11 @@
             </div>
         </section>
     </footer>
-    <script src="js/script.js"></script>
+    <script src="{{asset('js/script.js')}}"></script>
+    <script src="{{asset('js/cadastro.js')}}"></script>
+    <script src="{{asset('js/contato.js')}}"></script>
+    <script src="{{asset('js/produto.js')}}"></script>
+    <!-- <script src="{{asset('js/script.js')}}"></script> -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>

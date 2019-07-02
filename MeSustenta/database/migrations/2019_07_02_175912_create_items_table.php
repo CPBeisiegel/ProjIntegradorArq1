e9c12e01244d2479df7bc1f464146feb4727d306
AndreaRiveiro <49888116+AndreaRiveiro@users.noreply.github.com>
2019-07-02 15:07:04 -0300

    Criação de model e Migration ajuste Items

--- MeSustenta/database/migrations/2019_07_02_175912_create_items_table.php
+++ MeSustenta/database/migrations/2019_07_02_175912_create_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateItensTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
